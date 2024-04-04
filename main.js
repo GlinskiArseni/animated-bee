4e4ad944c13d31d62aa7e17a46503a1785ba8afe
arsenij <arsenij.glinski123@gmail.com>
2024-04-04 19:41:37 +0300

    added receipt

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
+/*(Russia)
+цифры в скобках(только в расписке) - строка 
+canvas(22) - ищет элемент в документе с id
+ctx(23) - он задает контекст в виде 2d формата
+функция circle(28) - создает круг по заданным кординатам(25,26) 
+функция drawBee(38) - рисует пчелу функцией circle
+функция update(50) - обновляет кординаты пчелы на рандомное число от -2 до 2
+и если он достигает больше 200 то возвращается на раньше заданные кординаты 
+либо если становится больше 0 то также возвращается на раньше заданные кординаты
+функция setInterval(61) - очищает поле и рисует пчелу каждые 30 милисекунд
+(English)
+numbers in brackets (only in receipt) - line
+canvas(22) - searches for an element in the document with id
+ctx(23) - it sets the context in 2d format
+function circle(28) - creates a circle at given coordinates(25,26)
+function drawBee(38) - draws a bee using the circle function
+function update(50) - updates the coordinates of the bee to a random number from -2 to 2
+and if it reaches more than 200 then it returns to the previously specified coordinates
+or if it becomes greater than 0, it also returns to the previously specified coordinates
+function setInterval(61) - clears the field and draws a bee every 30 milliseconds
+*/
 let  canvas = document.getElementById("canvas")
 let  ctx =  canvas.getContext("2d");
 
 let x = 100;
 let y = 100;
 
-var circle = function (x, y, radius, fillCircle) {
+let circle = function (x, y, radius, fillCircle) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, Math.PI * 2, false); 
   if (fillCircle) {
